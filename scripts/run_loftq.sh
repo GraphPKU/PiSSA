2a27b5e6fd51c34394c7045b1598567d4bb81250
Fanxu Meng <60565778+fxmeng@users.noreply.github.com>
2024-12-04 11:36:33 +0800

    Update run_loftq.sh

--- scripts/run_loftq.sh
+++ scripts/run_loftq.sh
@@ -5,9 +5,10 @@ OUTPUT_PATH="output/LoftQ-Llama-3-8B-4bit-64rank"
 DATA_PATH="meta-math/MetaMathQA"
 
 # batch size = per_device_train_batch_size * gradient_accumulation_steps * num_gpus = 128
-deepspeed --master_port=16971 --include=localhost:0 pissa.py \
+deepspeed --master_port=16971 --include=localhost:0 train.py \
     --deepspeed configs/ds_config_zero2_no_offload.json \
     --model_name_or_path $RESIDUAL_MODEL \
+    --full_finetune False \
     --bf16 \
     --bits 4 \
     --use_lora True \
